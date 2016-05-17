0c547a21eab21ef8994fdc453fb90b71e50a02f9
David M. Lee <leedm777@yahoo.com>
2016-05-17 14:28:12 -0500

    Added schema caching

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,14 @@
+import appdirs from 'appdirs';
 import fetch from 'node-fetch';
 import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+import { log } from './log';
 
 const URL = 'http://vstoolkit.amazonwebservices.com/CloudFormationSchema/CloudFormationV1.schema';
+const cacheDir = appdirs.userCacheDir('cloudinator', 'building5');
+const cacheFile = path.join(cacheDir, 'schemaCache.json');
 
 export async function getSchema({ url = URL, file } = {}) {
   let content;
@@ -17,9 +24,61 @@ export async function getSchema({ url = URL, file } = {}) {
       });
     });
   } else {
-    content = await fetch(url)
-      .then(res => res.text());
+    let cache = null;
+    try {
+      log.debug({ cacheFile }, 'loading cache');
+      cache = require(cacheFile);
+    } catch (ignored) {
+      // default empty cache
+      cache = {};
+    }
+
+    const cacheEntry = _.get(cache, url, {});
+
+    log.debug({ url }, 'loading schema');
+    const res = await fetch(url, {
+      headers: {
+        'If-None-Match': _.get(cacheEntry, 'etag'),
+      },
+    });
+
+    switch (res.status) {
+      case 200:
+        // cache miss
+        log.debug('schema cache miss');
+        cacheEntry.content = await res.json();
+        cacheEntry.etag = res.headers.get('ETag');
+        cache[url] = cacheEntry;
+
+        log.debug('schema updating cache');
+        try {
+          fs.mkdirSync(cacheDir);
+        } catch (err) {
+          // ignore EEXIST errors
+          if (err.code !== 'EEXIST') {
+            throw err;
+          }
+        }
+        fs.writeFileSync(cacheFile, JSON.stringify(cache));
+        break;
+      case 304:
+        // cache hit
+        log.debug('schema cache hit');
+        break;
+      default: {
+        const message = await res.text();
+        log.error({ res: _.pick(res, ['type', 'url', 'status', 'headers']), message },
+          'error loading schema');
+        throw new Error(`${res.status} error loading schema`);
+      }
+    }
+
+    content = cacheEntry.content;
   }
 
-  return JSON.parse(content);
+  if (_.isString(content)) {
+    content = JSON.parse(content);
+  }
+
+  return content;
 }
